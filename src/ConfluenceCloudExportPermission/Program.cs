3056cbdde4bcae6ea8428a81ba407077ccae113c
Roger Briggen <roger.briggen@bluewin.ch>
2020-02-08 18:37:33 +0100

    added microsoft console and debug logging

---
+++ src/ConfluenceCloudExportPermission/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConfluenceCloudExportPermissionLib;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ConfluenceCloudExportPermission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setup log with DI
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+            
+            logger.LogDebug("Application started");
+            
+            var myClass = serviceProvider.GetService<ExportZipFile>();
+
+            myClass.TestMethod();
+
+            logger.LogDebug("Application closing...");
+            serviceProvider.Dispose();
+
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            //we will configure logging here
+            services.AddLogging(configure => configure.AddConsole(consoleLoggerOptions => consoleLoggerOptions.TimestampFormat = "[dd.MM.yyyy HH:mm:ss.fff]")).Configure<LoggerFilterOptions>(cfg => cfg.MinLevel = LogLevel.Debug).AddTransient<ExportZipFile>();
+            services.AddLogging(configure => configure.AddDebug()).Configure<LoggerFilterOptions>(cfg => cfg.MinLevel = LogLevel.Debug).AddTransient<ExportZipFile>();
+        }
+
+        
+    }
+}
--- src/ConfluenceCloudExportPermissionLib/ExportZipFile.cs
+++ src/ConfluenceCloudExportPermissionLib/ExportZipFile.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
 
 namespace ConfluenceCloudExportPermissionLib
 {
+    
     public class ExportZipFile
     {
-        public ExportZipFile()
+        private readonly ILogger _logger;
+        public ExportZipFile(ILogger<ExportZipFile> logger)
         {
-
+            _logger = logger;
         }
 
         public bool TestMethod()
         {
+            _logger.LogDebug("TestMethod");
             return true;
         }
     }
